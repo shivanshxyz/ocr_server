ac6c432f312e7bf49c0555ff2106de7046d84bc1
rt2939 <aggarwal.ram24@gmail.com>
2020-07-18 01:29:51 +0530

    Body parser fixed

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const fs = require("fs");
 const bodyParser = require('body-parser');
 const Tesseract = require('tesseract.js');
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
  
 const PORT =  process.env.PORT || 3000  ;
 
